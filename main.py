1406a21783c7c0a9ed82efabfb3d7e2a65d91f69
Divyesh Rajput <114702639+rajputdi@users.noreply.github.com>
2023-10-02 16:58:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     st.title("APP PART 2(data profiling, great expectations) (Part 2)")
 
     st.write("Upload a file for this session:")
-    uploaded_file = uploader.file_uploader()
+    uploaded_file = Uploader.file_uploader()
 
     # More functionalities can be added as you specify further requirements.
 
