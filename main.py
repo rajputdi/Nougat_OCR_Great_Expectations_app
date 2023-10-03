71346af5d8c46a4596ee615ef1ccf7ac04fa7bb6
rajputdi <rajput.di@northeastern.edu>
2023-10-03 00:54:25 -0400

    reporting

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import streamlit as st
-from modules import Uploader, report_selector, data_processor
+from modules import (
+    Uploader,
+    report_selector,
+    data_processor,
+    data_validator,
+    data_corrector,
+)
 
 
 def main():
-    st.title("Streamlit App")
+    st.title("Streamlit App with Great Expectations")
 
     st.write("Upload a file for this session:")
     uploaded_file = Uploader.file_uploader()
-
     report_choice = report_selector.select_report_type()
 
-    # Process the data
     if uploaded_file:
         df = data_processor.process_txt(uploaded_file, report_choice)
 
-        # Add a button to preview the data
+        # Generate and display profiling report
+        html_report = data_validator.generate_profiling_report(df)
+        st.markdown(html_report, unsafe_allow_html=True)
+
+        # Add a button to preview the corrected data
         if st.button("View Data"):
             st.dataframe(df.head(50))  # Display top 50 rows upon button click
 
--- modules/data_validator.py
+++ modules/data_validator.py
@@ -2,6 +2,9 @@ import great_expectations as ge
 
 
 def initialize_expectations():
+    """
+    Initialize the expectation suite for the loan data.
+    """
     context = ge.data_context.DataContext()
     suite = context.create_expectation_suite("loan_data_expectations")
     context.save_expectation_suite(suite)
@@ -9,6 +12,9 @@ def initialize_expectations():
 
 
 def set_credit_score_expectation(suite):
+    """
+    Set the expectation for the Credit Score column.
+    """
     suite.add_expectation(
         {
             "expectation_type": "expect_column_values_to_be_between",
@@ -18,6 +24,9 @@ def set_credit_score_expectation(suite):
 
 
 def validate_data(df):
+    """
+    Validate the dataframe against the defined expectation suite.
+    """
     context = ge.data_context.DataContext()
     suite = context.get_expectation_suite("loan_data_expectations")
 
@@ -30,3 +39,22 @@ def validate_data(df):
     results = ge_df.validate(expectation_suite=suite)
 
     return results
+
+
+def generate_profiling_report(df):
+    """
+    Generate a data profiling report for the provided dataframe.
+    """
+    context = ge.data_context.DataContext()
+    suite = context.get_expectation_suite("loan_data_expectations")
+    ge_df = ge.from_pandas(df)
+    results = ge_df.validate(expectation_suite=suite)
+
+    # Render the results in an HTML format
+    html_report = (
+        ge.data_context.DataContext()
+        .build_data_docs()[0]["local_site"]["site_index_builder"]
+        .build()["full_static_asset_html_paths"][0]
+    )
+
+    return html_report
