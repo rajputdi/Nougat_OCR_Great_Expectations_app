af6a6d5f079a63475162cf26e2cf263bc7736ebb
rajputdi <rajput.di@northeastern.edu>
2023-10-02 23:04:00 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from modules import uploader, report_selector, data_processor
+from modules import Uploader, report_selector, data_processor
 
 
 def main():
     st.title("Streamlit App")
 
     st.write("Upload a file for this session:")
-    uploaded_file = uploader.file_uploader()
+    uploaded_file = Uploader.file_uploader()
 
     report_choice = report_selector.select_report_type()
 
