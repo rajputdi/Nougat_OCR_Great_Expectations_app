86bfcefc82871239468bc99246f4fb4dbbf495c7
rajputdi <rajput.di@northeastern.edu>
2023-10-03 19:14:16 -0400

    ge changes

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ import streamlit as st
 from modules import Uploader, data_processor, data_validator
 
 
+def display_validation_results(df):
+    validation_results = data_validator.set_or_update_expectations(df)
+
+    # Extracting statistics from the validation results for display
+    for batch in validation_results["results"]:
+        batch_name = batch.batch_identifier["batch_identifier"]
+        expectation_results = (
+            batch.expectation_suite_validation_result.expectation_results
+        )
+
+        # Display the results in Streamlit
+        st.write(f"## Validation Results for {batch_name}")
+        for expectation_result in expectation_results:
+            st.write(expectation_result.expectation_config.expectation_type)
+            st.write("Success:", expectation_result.success)
+            if not expectation_result.success:
+                st.write("Details:", expectation_result.result)
+
+
 def main():
     st.title("Streamlit App with Great Expectations")
 
@@ -11,18 +30,13 @@ def main():
     if uploaded_file:
         df = data_processor.process_txt(uploaded_file)
 
-        # Initialize and set expectations before generating the report
-        suite = data_validator.initialize_expectations()
-        data_validator.set_credit_score_expectation(suite)
-
         # Display the dataframe (Top 50 rows) if the "View Data" button is clicked
         if st.button("View Data"):
             st.dataframe(df.head(50))
 
-        # Generate and display the GE profiling report if the respective button is clicked
-        if st.button("View GE Profiling Report"):
-            html_url = data_validator.generate_profiling_report(df)
-            st.markdown(f"[Click here to view the GE Profiling Report]({html_url})")
+        # Validate the data and display the results
+        if st.button("Validate Data"):
+            display_validation_results(df)
 
 
 if __name__ == "__main__":
--- modules/data_validator.py
+++ modules/data_validator.py
@@ -1,39 +1,23 @@
 import great_expectations as ge
-from great_expectations.core.expectation_configuration import ExpectationConfiguration
 
 
-def initialize_expectations():
+def set_or_update_expectations(df):
     context = ge.data_context.DataContext()
-    try:
-        suite = context.get_expectation_suite("loan_data_expectations")
-    except ge.exceptions.exceptions.DataContextError:
-        suite = context.create_expectation_suite("loan_data_expectations")
-        context.save_expectation_suite(suite)
-    return suite
-
-
-def set_credit_score_expectation(suite):
-    expectation_configuration = ExpectationConfiguration(
-        expectation_type="expect_column_values_to_be_between",
-        kwargs={"column": "Credit Score", "min_value": 300, "max_value": 850},
-    )
-    suite.add_expectation(expectation_configuration)
-
-
-def generate_profiling_report(df):
-    context = ge.data_context.DataContext()
-    suite = context.get_expectation_suite("loan_data_expectations")
+    suite_name = "loan_data_expectations"
+    suite = context.create_expectation_suite(suite_name, overwrite_existing=True)
     ge_df = ge.from_pandas(df)
+    batch = ge.dataset.Batch(data=ge_df, batch_request=None, expectation_suite=suite)
 
-    batch_kwargs = {"dataset": ge_df, "datasource": "pandas_datasource"}
-    batch = context.get_batch(batch_kwargs, suite)
-    results = context.run_validation_operator(
+    # Example expectation: Credit Score between 300 and 850
+    batch.expect_column_values_to_be_between("Credit Score", 300, 850, mostly=0.95)
+
+    # ... add other expectations as necessary ...
+
+    # Save the updated suite
+    context.save_expectation_suite(batch.get_expectation_suite(), suite_name)
+
+    # Validate the batch against the suite
+    validation_results = context.run_validation_operator(
         "action_list_operator", assets_to_validate=[batch]
     )
-    validation_results_dict = results.results[batch.batch_id][
-        "expectation_suite_results"
-    ]
-    document = context.build_data_docs(resource_identifiers=[validation_results_dict])
-    site_keys = list(context.get_config().data_docs_sites.keys())
-    html_url = document[site_keys[0]]
-    return html_url
+    return validation_results
