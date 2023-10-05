6c1401528ff271d73a89883154755597b9784ea9
rajputdi <rajput.di@northeastern.edu>
2023-10-04 23:17:53 -0400

    Update ge_validator.py

--- modules/ge_validator.py
+++ modules/ge_validator.py
@@ -4,7 +4,7 @@ from great_expectations.data_context.types.base import DataContextConfig
 
 def get_in_memory_data_context():
     """
-    This creates a new GE DataContext with an in-memory backend.
+    This creates a basic in-memory GE DataContext.
     """
     data_context_config = {
         "datasources": {
@@ -16,16 +16,10 @@ def get_in_memory_data_context():
                 "class_name": "PandasDatasource",
                 "module_name": "great_expectations.datasource",
             }
-        },
-        "stores": {
-            "my_expectation_store": {"class_name": "InMemoryStoreBackend"},
-            "validations_store": {"class_name": "InMemoryStoreBackend"},
-        },
-        "expectations_store_name": "my_expectation_store",
-        "validations_store_name": "validations_store",
+        }
     }
 
-    context = ge.data_context.DataContext(project_config=data_context_config)
+    context = ge.data_context.DataContext(config=data_context_config)
     return context
 
 
