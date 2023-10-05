483d8877a8ac0102f87ad8337c77922591080dff
rajputdi <rajput.di@northeastern.edu>
2023-10-05 18:43:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -49,15 +49,20 @@ def main():
             retrieved_checkpoint = context.get_checkpoint(name="fm_checkpoint_v1")
             st.write(retrieved_checkpoint)
 
-            ge_df = ge.dataset.PandasDataset(df)
-            results = context.run_checkpoint(
-                checkpoint_name="fm_checkpoint_v1",
-                batch_request={
-                    "batch_data": ge_df,
-                    "datasource_name": "my_pandas_datasource1",
-                    "data_asset_name": "fm_dataframe",
-                },
+            suite = context.get_expectation_suite(
+                suite_name="freddie_mac_expectation_suite"
             )
+            results = ge_df.validate(expectation_suite=suite)
+
+            # ge_df = ge.dataset.PandasDataset(df)
+            # results = context.run_checkpoint(
+            #     checkpoint_name="fm_checkpoint_v1",
+            #     batch_request={
+            #         "batch_data": ge_df,
+            #         "datasource_name": "my_pandas_datasource1",
+            #         "data_asset_name": "fm_dataframe",
+            #     },
+            # )
             st.write(results)
 
 
