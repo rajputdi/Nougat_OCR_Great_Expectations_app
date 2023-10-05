f2fe57fba2f21c1ff334b6eb69c940d3c53c8330
rajputdi <rajput.di@northeastern.edu>
2023-10-05 19:21:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def main():
             retrieved_checkpoint = context.get_checkpoint(name="fm_checkpoint_v1")
             st.write(retrieved_checkpoint)
 
-            # suite = context.get_expectation_suite("freddie_mac_expectation_suite")
-            # results = ge_df.validate(expectation_suite=suite)
+            suite = context.get_expectation_suite("freddie_mac_expectation_suite")
+            results = ge_df.validate(expectation_suite=suite)
 
             # ge_df = ge.dataset.PandasDataset(df)
             # results = context.run_checkpoint(
