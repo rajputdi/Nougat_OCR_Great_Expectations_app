7080a13ef122581e0e3d6c6cee6691eae5566780
rajputdi <rajput.di@northeastern.edu>
2023-10-04 20:52:44 -0400

    Update data_validator.py

--- modules/data_validator.py
+++ modules/data_validator.py
@@ -1,6 +1,10 @@
 import great_expectations as ge
 from great_expectations.data_context import BaseDataContext
 import datetime
+from great_expectations.data_context.types.base import DataContextConfig
+from great_expectations.data_context import BaseDataContext
+from great_expectations.data_context import store
+from great_expectations.data_context.store import InMemoryStoreBackend
 import os
 
 
@@ -16,12 +20,65 @@ def set_schema_expectations(df):
     # ... add other type checks as needed
 
 
+def create_data_context_config():
+    config = DataContextConfig(
+        datasources={
+            "my_datasource": {
+                "data_asset_type": {
+                    "class_name": "PandasDataset",
+                    "module_name": "great_expectations.dataset",
+                },
+                "batch_kwargs_generators": {},
+                "class_name": "PandasDatasource",
+                "module_name": "great_expectations.datasource",
+            }
+        },
+        store_backend_defaults=InMemoryStoreBackend(),
+        validation_operators={
+            "action_list_operator": {
+                "class_name": "ActionListValidationOperator",
+                "action_list": [
+                    {
+                        "name": "store_validation_result",
+                        "action": {"class_name": "StoreValidationResultAction"},
+                    },
+                    {
+                        "name": "store_evaluation_params",
+                        "action": {"class_name": "StoreEvaluationParametersAction"},
+                    },
+                    {
+                        "name": "update_data_docs",
+                        "action": {"class_name": "UpdateDataDocsAction"},
+                    },
+                ],
+            }
+        },
+        data_docs_sites={
+            "local_site": {
+                "class_name": "SiteBuilder",
+                "store_backend": {
+                    "class_name": "TupleFilesystemStoreBackend",
+                    "base_directory": "uncommitted/data_docs/local_site/",
+                },
+                "site_index_builder": {
+                    "class_name": "DefaultSiteIndexBuilder",
+                },
+            }
+        },
+    )
+
+    return BaseDataContext(project_config=config)
+
+
 def validate_dataframe(df):
     # Convert the DataFrame to a GE dataset
     ge_df = ge.from_pandas(df)
 
     # First, set the schema expectations
     set_schema_expectations(ge_df)
+
+    # Create a new data context
+    context = create_data_context_config()
 
     # Validate the dataframe
     results = ge_df.validate()
