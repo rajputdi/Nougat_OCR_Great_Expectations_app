104550148116dfe73e0fd2c56226910b2ed38c56
rajputdi <rajput.di@northeastern.edu>
2023-10-06 09:24:00 -0400

    Update home.py

--- home.py
+++ home.py
@@ -2,19 +2,15 @@ import streamlit as st
 from pages.main import main
 from pages.diagram import diagram
 
-
-def home():
-    st.title("DAMG 7245 - Assignment 01-Part 2-Team 4")
-    st.sidebar.success("Select from the dropdown above☝")
-
-
 page_names_to_funcs = {
-    "Home": home,
+    "Home": lambda: st.title("DAMG 7245 - Assignment 01-Part 2-Team 4"),
     "Streamlit App": main,
     "Architecture": diagram,
 }
 
-# Place this line at the top, before any other Streamlit calls
-demo_name = st.sidebar.selectbox("Choose an option", page_names_to_funcs.keys())
-# Invoke the corresponding function
+# Ensure the sidebar-specific calls are made first
+demo_name = st.sidebar.selectbox("Choose an option", list(page_names_to_funcs.keys()))
+st.sidebar.success("Select from the dropdown above☝")
+
+# Then call the main page function
 page_names_to_funcs[demo_name]()
