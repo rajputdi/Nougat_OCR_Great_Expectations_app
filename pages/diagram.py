c227b81dd26c684ce6edec6a34378830abc1389e
rajputdi <rajput.di@northeastern.edu>
2023-10-06 09:54:25 -0400

    Update diagram.py

--- pages/diagram.py
+++ pages/diagram.py
@@ -4,4 +4,4 @@ from PIL import Image
 
 def diagram():
     image = Image.open("pages/part2_architecture.png")
-    st.image(image, caption="Architecture Diagram", width=700, height=700)
+    st.image(image, caption="Architecture Diagram", width=700)
